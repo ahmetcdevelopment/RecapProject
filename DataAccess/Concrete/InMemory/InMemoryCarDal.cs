9ce7f930f30dde37d9dfbe2ee29f02e2b29e923d
DELL <DELL@DESKTOP-4R2VU1K>
2022-05-18 12:25:22 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,58 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            
+            _cars = new List<Car>
+            {
+                new Car{Id = 1, BrandId = 1,ColorId=11,DailyPrice=1500,ModelYear=2011},
+                new Car{Id = 2, BrandId = 3,ColorId=22,DailyPrice=1900,ModelYear=2012},
+                new Car{Id = 3, BrandId = 3,ColorId=33,DailyPrice=1200,ModelYear=2013}
+
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete;
+            carToDelete = _cars.SingleOrDefault(c=>c.Id==car.Id);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate;
+            carToUpdate = _cars.SingleOrDefault(c=>c.Id==car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+        }
+
+        List<Car> ICarDal.GetById(int id)
+        {
+            return _cars.Where(c=>c.BrandId==id).ToList();
+        }
+    }
+}
---
+++ RecapProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace RecapProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carmanager = new CarManager(new InMemoryCarDal());
+            foreach (var car in carmanager.GetAll())
+            {
+                Console.WriteLine(car.DailyPrice);
+            }
+        }
+    }
+}
